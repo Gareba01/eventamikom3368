30b39032915bb742a3109cb3a59c80b2576aacee
Gareba01 <malwimarom@gmail.com>
2026-04-17 10:44:16 +0700

    tugas2

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,27 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\TicketController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route Detail Event
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
 
+// Route Checkout
+Route::get('/checkout', [EventController::class, 'checkout'])->name('events.checkout');
+
+// Route Tiket
+Route::get('/ticket', [EventController::class, 'ticket'])->name('tickets.index');
+
+
+// 1. Rute Home - Pakai HomeController saja, hapus yang Route::get('/', function...)
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+
+
+// 3. Rute Statis Lainnya
 Route::get('/tentang', function () {
     return '<h1>Ini adalah Halaman Tentang Aplikasi Event Hub (Running on Mac)</h1>';
 });
@@ -14,17 +30,15 @@ Route::get('/kontak', function () {
     return view('contact');
 });
 
-// Rute untuk Halaman Profil
 Route::get('/profil', function () {
     return view('profil');
 });
 
-// Rute untuk Halaman Katalog
 Route::get('/katalog', function () {
     return view('katalog');
 });
 
-// Rute untuk Halaman Bantuan
 Route::get('/bantuan', function () {
     return view('bantuan');
 });
+
